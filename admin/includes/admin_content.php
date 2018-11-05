a1e6348ee17e65d542ff0d276f79cd8a50552ca3
Luis E <bellyluis@gmail.com>
2018-11-04 22:14:07 -0500

    User class and autoload function

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -8,11 +8,39 @@
                 </h1>
                 <?php
                   
-                  $sql = "SELECT * FROM users WHERE id = 1";
-                  $result  = $database->query($sql);
-                  $user_found = mysqli_fetch_array($result);
+            /*
+                    $result_set= User::find_user_by_id(2);
 
-                  echo $user_found['username'];
+                    while($row = mysqli_fetch_array($result_set)){
+
+                        echo $row['username'];
+                    }
+
+                        */
+
+                     /*   $found_user = User::find_user_by_id(2);
+
+                       $user = User::instantation($found_user);
+
+                        echo $user->username;
+
+                   */
+/*
+                    $users= User::find_all_users();
+                   
+                    foreach($users as $user){
+
+                        echo $user->username . "<br>";
+
+                    }*/
+                  
+
+                    $found_user = User::find_user_by_id(2);
+
+                    echo $found_user->username;
+
+                 
+
 
                 ?>
             <ol class="breadcrumb">
